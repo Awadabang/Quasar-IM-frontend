c6a2414883c8247dccc81498792b06b7f3a551a9
Awadabang <1765256254@qq.com>
2022-02-22 13:52:22 +0800

    feat: add interface:Conversations

--- src/components/models.ts
+++ src/components/models.ts
@@ -11,3 +11,12 @@ export interface User_State {
   username: string;
   token: string;
 }
+
+export interface Conversations {
+  id: number;
+  person: string;
+  avatar: string;
+  caption: string;
+  time: string;
+  sent: boolean;
+}
